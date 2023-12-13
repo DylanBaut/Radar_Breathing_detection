ec212c235a30dd60f38d96930487fe99d823473e
Dylan Bautista <70183425+DylanBaut@users.noreply.github.com>
2023-12-12 18:52:52 -0600

    Add files via upload

---
+++ udp_server.py
@@ -0,0 +1,17 @@
+import socket
+
+# Binding to 0.0.0.0 allows UDP connections to any address
+# that the device is using
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5000
+
+sock = socket.socket(socket.AF_INET, # Use Internet socket family
+                     socket.SOCK_DGRAM) # Use UDP packets
+addr = (UDP_IP, UDP_PORT)
+
+sock.bind(addr)
+print('Socket is bound to', addr)
+
+while True:
+    data, addr = sock.recvfrom(1024)    
+    print('Received message:', data)
